2d0f4a3f1c8d95b7e7d25a3c4376504404755237
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-11-16 19:49:53 +0200

    base: Reboot Recovery & Reboot Bootloader in Power Menu
    Coded fram scratch by me (yashade2001). It's not a port from CM11 or other KK ROMs.
    
    Drawables by Alex Cruz
    
    PS2:
    -use same logic as in 4.4 when to enable advanced reboot
    -compared to 4.4 dont implement in ShutdownThread but as a submenu of the global actions menu
    -expose advanced reboot API from ShutdownThread
    -add new GlobalAction filter for checking primary user and secure locksreens
    
    PS6:
    -new drawables
    
    PS7:
    -slient mode button sets no interruptions
    
    Change-Id: I3e60ce93a541b0c9deb7ad53b27825acce91d251

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -168,12 +168,26 @@ public final class ShutdownThread extends Thread {
             resourceId = com.android.internal.R.string.reboot_confirm;
         }
 
+        int messageResourceId = longPressBehavior == 2
+                        ? com.android.internal.R.string.shutdown_confirm_question
+                        : com.android.internal.R.string.shutdown_confirm;
+        if (mRebootSafeMode) {
+            messageResourceId = com.android.internal.R.string.reboot_safemode_confirm;
+        } else if (mReboot) {
+            messageResourceId = com.android.internal.R.string.reboot_confirm;
+        }
         Log.d(TAG, "Notifying thread to start shutdown longPressBehavior=" + longPressBehavior);
 
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
+            }
+            int titleResourceId = com.android.internal.R.string.power_off;
+            if (mRebootSafeMode) {
+                titleResourceId = com.android.internal.R.string.reboot_safemode_title;
+            } else if (mReboot) {
+                titleResourceId = com.android.internal.R.string.reboot_title;
             }
             sConfirmDialog = new AlertDialog.Builder(context)
                     .setTitle(mRebootSafeMode
@@ -184,6 +198,9 @@ public final class ShutdownThread extends Thread {
                     .setMessage(resourceId)
                     .setPositiveButton(com.android.internal.R.string.yes,
                             new DialogInterface.OnClickListener() {
+                    .setTitle(titleResourceId)
+                    .setMessage(messageResourceId)
+                    .setPositiveButton(com.android.internal.R.string.yes, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             beginShutdownSequence(context);
                         }
@@ -297,6 +314,21 @@ public final class ShutdownThread extends Thread {
 
             pd.show();
         }
+
+        // throw up an indeterminate system dialog to indicate radio is
+        // shutting down.
+        ProgressDialog pd = new ProgressDialog(context);
+        if (mReboot) {
+            pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
+        } else {
+            pd.setTitle(context.getText(com.android.internal.R.string.power_off));
+        }
+        pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
+        pd.setIndeterminate(true);
+        pd.setCancelable(false);
+        pd.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+
+        pd.show();
 
         sInstance.mContext = context;
         sInstance.mPowerManager = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
