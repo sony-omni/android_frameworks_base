e33adc7568b684583ed57eefcd261fc255a85e8b
Roman Birg <roman@cyngn.com>
2015-02-15 13:37:15 +0700

    SystemUI: make long pressing recent switch to last app
    ATTENTION: using the static methods from TaskUtils requires
    now android.permission.INTERACT_ACROSS_USERS_FULL permission!
    
    With this patch, long pressing the recents key on the navigation bar
    will attempt to switch to your last application in the activity stack
    before the active one.
    
    The user's current home launcher package is ignored as well as SystemUI (so that
    RecentsActivity does not influence the behavior).
    
    Change-Id: I38771e0fce16b55bb5186af47115b4e812cb60b0
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    
    SystemUI: unbreak accesibility longpress Overview to unpin
    add a check to see if were in a locked app so not to hijack recents
    
    allow last app action to be assigned to hardware keys
    Also fix multi-user handling for 'switch to last app' and 'kill
    foreground app' actions.
    
    Change-Id: I1fb586d19b1198e6888b0f11e0a2813acf0bcd5b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Danny Baumann <dannybaumann@web.de>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    
    implement on top our current TaskUtils
    adding a nice animation when switching apps
    
    Change-Id: I6313903ac3c8f404c2e361446bef505eceba9117

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -37,6 +37,7 @@ import android.animation.TimeInterpolator;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
+import android.app.ActivityOptions;
 import android.app.IActivityManager;
 import android.app.Notification;
 import android.app.PendingIntent;
@@ -45,6 +46,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
@@ -116,6 +119,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.omni.TaskUtils;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
@@ -4100,6 +4104,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void handleLongPressBackRecents(View v) {
         try {
             boolean sendBackLongPress = false;
+            boolean hijackRecentsLongPress = false;
             IActivityManager activityManager = ActivityManagerNative.getDefault();
             boolean isAccessiblityEnabled = mAccessibilityManager.isEnabled();
             if (activityManager.isInLockTaskMode() && !isAccessiblityEnabled) {
@@ -4113,12 +4118,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     // If we aren't pressing recents right now then they presses
                     // won't be together, so send the standard long-press action.
                     sendBackLongPress = true;
+                } else if ((v.getId() == R.id.recent_apps) && !activityManager.isInLockTaskMode()) {
+                    hijackRecentsLongPress = true;
                 }
                 mLastLockToAppLongPress = time;
             } else {
                 // If this is back still need to handle sending the long-press event.
                 if (v.getId() == R.id.back) {
                     sendBackLongPress = true;
+                } else if ((v.getId() == R.id.recent_apps) && !activityManager.isInLockTaskMode()) {
+                    hijackRecentsLongPress = true;
                 } else if (isAccessiblityEnabled && activityManager.isInLockTaskMode()) {
                     // When in accessibility mode a long press that is recents (not back)
                     // should stop lock task.
@@ -4129,6 +4138,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 KeyButtonView keyButtonView = (KeyButtonView) v;
                 keyButtonView.sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.FLAG_LONG_PRESS);
                 keyButtonView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
+            }
+            if (hijackRecentsLongPress) {
+                TaskUtils.toggleLastApp(mContext, mCurrentUserId);
             }
         } catch (RemoteException e) {
             Log.d(TAG, "Unable to reach activity manager", e);
